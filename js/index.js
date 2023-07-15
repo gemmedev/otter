50e46f50e8065cf86b28653228c2e6842beaff79
Mauricio Vasco Vélez <maurovasco91@gmail.com>
2023-07-15 14:02:08 -0500

    inicio-java-script

---
+++ js/index.js
@@ -0,0 +1,16 @@
+/* var nombre = prompt('Escribe tu nombre');
+var apellido = prompt('Escribe tu apellido');
+var nombreCompleto = nombre + ' ' + apellido;
+alert('tu nombre completo es ' + nombreCompleto); */
+
+var num = parseInt(prompt('Ingrese un numero'));
+
+if (num <= 5) {
+  alert('el numero es menor a 5');
+} else if (num >= 5 && num <= 10) {
+  alert('el numero esta entre 5 y 10');
+} else {
+  alert('el numero es mayor a 11');
+}
+
+// parseInt(prompt('Teclea un  número'));
