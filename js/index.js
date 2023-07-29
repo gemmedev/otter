c920b57a66a6fdfc07348380ccb9aa033287dc28
Kevin Corrales <kevincorralesmora@hotmail.com>
2023-07-29 11:51:25 -0500

    logica-javascript

--- js/index.js
+++ js/index.js
@@ -3,14 +3,77 @@ var apellido = prompt('Escribe tu apellido');
 var nombreCompleto = nombre + ' ' + apellido;
 alert('tu nombre completo es ' + nombreCompleto); */
 
-var num = parseInt(prompt('Ingrese un numero'));
+/* var num = parseInt(prompt('Ingrese un numero')); */
 
-if (num <= 5) {
+/* if (num <= 5) {
   alert('el numero es menor a 5');
 } else if (num >= 5 && num <= 10) {
   alert('el numero esta entre 5 y 10');
 } else {
   alert('el numero es mayor a 11');
-}
+} */
 
 // parseInt(prompt('Teclea un  número'));
+
+/* ejercicio #1
+var edad = parseInt(prompt('Digita tu edad'));
+
+if (edad >= 18) {
+  alert('Es mayor de edad');
+} else {
+  alert('La edad es incorrecta');
+} */
+
+/* var color = prompt('Digita un color');
+
+if (color === 'rojo') {
+  alert('Color valido');
+}else if (color === 'azul') {
+  alert('Color valido');
+}else if (color === 'amarillo') {
+  alert('Color valido');
+}else if (color === 'verde') {
+  alert('Color valido');
+}else if (color === 'rosa') {
+  alert('Color valido');
+}else if (color === 'negro') {
+  alert('Color valido');
+}else {
+  alert('El color no es valido');
+} */ 
+
+/* ejercicio #2
+var color = prompt('Digita un color');
+
+if (color === 'rojo' | color === 'azul'  | color === 'amarillo'  | color === 'verde'  | color === 'rosa'  | color === 'negro') {
+  alert('Color valido');
+}else {
+  alert ('el color no es valido');
+} */
+
+/* ejercicio #3
+
+var num1 = parseInt(prompt('Escribe un número'));
+var num2 = parseInt(prompt('Escribe otro número'));
+
+if (num1 > num2) {
+  alert (num1 + ' es el numero mayor');
+}else if (num2 > num1) {
+  alert (num2 + ' es el numero mayor');
+}else {
+  alert ('Ambos numeros son iguales');
+} */
+
+
+
+var texto = prompt('Escriba algo');
+var valor = texto.charAt(0);
+alert(typeof(parseInt(valor)));
+
+/* if (isLetter(valor)) {
+  alert('el texto empieza con una letra');
+}else if (isNumber(valor)) {
+  alert('el texto empieza con un numero');
+}else {
+  alert('caracter desconocido');
+} */
